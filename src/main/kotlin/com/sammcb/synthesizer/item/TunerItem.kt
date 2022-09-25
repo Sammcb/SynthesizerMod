659cb489db18137e7e601242b368dad4fbc37815
Samuel McBroom <sam@sammcb.com>
2022-09-25 12:27:16 -0700

    Add config options to disable tiers and adjust delay between item creation. (#1)

--- src/main/kotlin/com/sammcb/synthesizer/item/TunerItem.kt
+++ src/main/kotlin/com/sammcb/synthesizer/item/TunerItem.kt
@@ -4,6 +4,7 @@ import com.sammcb.synthesizer.Constants
 import com.sammcb.synthesizer.block.Blocks
 import com.sammcb.synthesizer.block.SynthesizerBlock
 import com.sammcb.synthesizer.block.enums.Tier
+import com.sammcb.synthesizer.config.Config
 import net.minecraft.client.item.TooltipContext
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
@@ -25,12 +26,13 @@ class TunerItem(settings: Settings): Item(settings) {
 		if (!state.isOf(Blocks.SYNTHESIZER)) return ActionResult.FAIL
 
 		val currentTier = state.get(SynthesizerBlock.TIER)
-		val nextTier = Tier.values().firstOrNull { it.ordinal == currentTier.ordinal + 1} ?: Tier.WOOD
+		val nextTier = Tier.values().firstOrNull { it.ordinal > currentTier.ordinal && it.stackSize() > 0 } ?: Tier.WOOD
 		world.setBlockState(pos, state.with(SynthesizerBlock.TIER, nextTier))
 
 		val player = context.getPlayer()
 		if (player != null) {
-			player.sendMessage(Text.translatable("item.${Constants.MOD_ID}.tuner.tune", nextTier.stackSize()), true)
+			val ticks = if (Config.delay() < 0) 0 else Config.delay()
+			player.sendMessage(Text.translatable("item.${Constants.MOD_ID}.tuner.tune", nextTier.stackSize(), ticks), true)
 		}
 
 		world.playSound(null, pos, SoundEvents.BLOCK_AMETHYST_BLOCK_PLACE, SoundCategory.PLAYERS, 1f, 1f)
@@ -39,8 +41,9 @@ class TunerItem(settings: Settings): Item(settings) {
 	}
 
 	override fun appendTooltip(itemStack: ItemStack, world: World?, tooltip: MutableList<Text>, context: TooltipContext) {
+		val ticks = if (Config.delay() < 0) 0 else Config.delay()
 		tooltip.add(Text.translatable("item.${Constants.MOD_ID}.tuner.tooltip_1").formatted(Formatting.DARK_PURPLE))
 		tooltip.add(Text.translatable("item.${Constants.MOD_ID}.tuner.tooltip_2").formatted(Formatting.DARK_PURPLE))
-		tooltip.add(Text.translatable("item.${Constants.MOD_ID}.tuner.tooltip_3").formatted(Formatting.DARK_PURPLE))
+		tooltip.add(Text.translatable("item.${Constants.MOD_ID}.tuner.tooltip_3", ticks).formatted(Formatting.DARK_PURPLE))
 	}
 }
